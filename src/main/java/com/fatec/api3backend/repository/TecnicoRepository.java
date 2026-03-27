443c56e952e2817319182b2c3b03240c34be395c
JPM1 <menegasso00@gmail.com>
2026-03-27 20:30:33 -0300

    fix(#U01.1B): adaptação do pom.xml e application.properties para receber o banco

--- src/main/java/com/fatec/api3backend/repository/TecnicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.api3backend.repository;
-
-import com.fatec.api3backend.model.Tecnico; // Isso aqui diz pro Java onde está o Tecnico
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
-}
