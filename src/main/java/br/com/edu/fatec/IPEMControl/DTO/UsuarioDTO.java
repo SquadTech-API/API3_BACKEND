958e6d7733217159d278843194a69e8db3979500
JhowRossi <137461345+JhowRossii@users.noreply.github.com>
2026-03-29 13:55:27 -0300

    refactor(#U01B) - Refatoração com integração do front
    Foi realizada a refatoração da classe usuario e seus afins  para que a requisição com front consiga ser efetuada.

--- src/main/java/br/com/edu/fatec/IPEMControl/DTO/UsuarioDTO.java
+++ src/main/java/br/com/edu/fatec/IPEMControl/DTO/UsuarioDTO.java
@@ -1,13 +1,21 @@
 package br.com.edu.fatec.IPEMControl.DTO;
 
+import br.com.edu.fatec.IPEMControl.Entities.Usuario;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class UsuarioDTO {
-    private Integer matricula;
-    private String nomeCompleto;
-    private String email;
-    private String cargo;
-    private String senha; // senha pura, vai ser criptografada no service
-}
 
+    private String cpf;
+    private String numeroHabilitacao;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String email;
+    private String senha;
+    private Usuario.TipoUsuario tipoUsuario;
+    private String cargo;
+    private Boolean colaboradorAtivo;
+    private Usuario.TipoHabilitacao tipoHabilitacao;
+}
