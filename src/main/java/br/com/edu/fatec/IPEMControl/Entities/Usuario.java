240356e50848cc00f1583d998864b08b5dd6044a
JhowRossi <137461345+JhowRossii@users.noreply.github.com>
2026-04-05 11:01:28 -0300

    refactor(#00) - refatoração da entidade usuario
    Foi refatorada a entidade usuario para que a matricula seja gerada automaticamente , assim conseguindo fazer a integração com a inserção de dados vindo do frontend.

--- src/main/java/br/com/edu/fatec/IPEMControl/Entities/Usuario.java
+++ src/main/java/br/com/edu/fatec/IPEMControl/Entities/Usuario.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class Usuario {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "matricula")
     private Integer matricula;
 
