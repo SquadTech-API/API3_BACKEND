c660ff33ab75e985772e4bf99c5d5525ce871999
Manuela Brito <manuelamigre@gmail.com>
2026-04-20 17:45:07 -0300

    Revert "feat(#A05):Implementação da estrutura de relatórios de uso da frota"
    This reverts commit 402f6e1a64a492d072d46f7cb82cbbabfdc55a88, reversing
    changes made to deacdfeb56a4466532f88612494bba482c7eae21.

--- src/main/java/br/com/edu/fatec/IPEMControl/Repository/TecnicoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.edu.fatec.IPEMControl.Repository;
-
-import br.com.edu.fatec.IPEMControl.Entities.Tecnico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
-}
