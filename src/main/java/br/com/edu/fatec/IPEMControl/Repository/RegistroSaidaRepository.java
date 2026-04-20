f3d09c2ce96ab149bc65518cbd65c7ce0dcca5bb
JPM1 <menegasso00@gmail.com>
2026-04-20 16:35:48 -0300

    feat(#A06.2B): implementação de consultas no RegistroSaidaRepository

--- src/main/java/br/com/edu/fatec/IPEMControl/Repository/RegistroSaidaRepository.java
+++ src/main/java/br/com/edu/fatec/IPEMControl/Repository/RegistroSaidaRepository.java
@@ -4,6 +4,8 @@ import br.com.edu.fatec.IPEMControl.Entities.RegistroSaida;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +19,9 @@ public interface RegistroSaidaRepository extends JpaRepository<RegistroSaida, In
 
     Optional<RegistroSaida> findTopByUsuarioMatriculaAndStatusOrderByDataHoraSaidaDesc(
             Integer matricula, String status);
+    List<RegistroSaida> findByUsuarioMatriculaAndDataHoraSaidaBetween(
+            Integer matricula,
+            LocalDateTime inicioDia,
+            LocalDateTime fimDia
+    );
 }
