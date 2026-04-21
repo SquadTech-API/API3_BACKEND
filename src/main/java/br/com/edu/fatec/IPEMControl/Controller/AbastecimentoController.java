cdfb8fb87703cee21313f7d5dcb2c340538c5575
pedroquirino <pedrohpquirino@gmail.com>
2026-04-20 22:09:52 -0300

    feat(A04): Historico de Abastecimento

--- src/main/java/br/com/edu/fatec/IPEMControl/Controller/AbastecimentoController.java
+++ src/main/java/br/com/edu/fatec/IPEMControl/Controller/AbastecimentoController.java
@@ -1,15 +1,16 @@
 package br.com.edu.fatec.IPEMControl.Controller;
 
 import br.com.edu.fatec.IPEMControl.DTO.AbastecimentoDTO;
+import br.com.edu.fatec.IPEMControl.DTO.AbastecimentoHistoricoDTO;
 import br.com.edu.fatec.IPEMControl.Entities.Abastecimento;
 import br.com.edu.fatec.IPEMControl.Service.AbastecimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-// CORRIGIDO: era /abastecimento (singular) mas o JS chama /abastecimento (mantido singular)
-// Deixamos singular para bater com o abastecimento.js corrigido que chama /abastecimento
 @RequestMapping("/abastecimento")
 public class AbastecimentoController {
 
@@ -19,5 +20,11 @@ public class AbastecimentoController {
     @PostMapping
     public ResponseEntity<Abastecimento> criar(@RequestBody AbastecimentoDTO dto) {
         return ResponseEntity.status(201).body(abastecimentoService.salvar(dto));
+    }
+
+    @GetMapping("/historico")
+    public ResponseEntity<List<AbastecimentoHistoricoDTO>> historico(
+            @RequestParam(required = false) Integer idVeiculo) {
+        return ResponseEntity.ok(abastecimentoService.listarHistorico(idVeiculo));
     }
 }
--- src/main/java/br/com/edu/fatec/IPEMControl/Repository/AbastecimentoRepository.java
+++ src/main/java/br/com/edu/fatec/IPEMControl/Repository/AbastecimentoRepository.java
@@ -4,10 +4,15 @@ import br.com.edu.fatec.IPEMControl.Entities.Abastecimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AbastecimentoRepository extends JpaRepository<Abastecimento, Integer> {
 
     Optional<Abastecimento> findTopByRegistroSaidaVeiculoIdVeiculoOrderByDataHoraDesc(Integer idVeiculo);
+
+    List<Abastecimento> findByRegistroSaidaVeiculoIdVeiculoOrderByDataHoraDesc(Integer idVeiculo);
+
+    List<Abastecimento> findAllByOrderByDataHoraDesc();
 }
