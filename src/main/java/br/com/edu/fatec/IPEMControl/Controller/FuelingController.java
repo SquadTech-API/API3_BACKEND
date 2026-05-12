6c701bc86cdf92076e86301d8a3660d77fd7974a
Manuela Brito <manuelamigre@gmail.com>
2026-05-12 10:22:40 -0300

    refactor(#03): Refactorada a classe AbastecimentoController p/ FuellingController

---
+++ src/main/java/br/com/edu/fatec/IPEMControl/Controller/FuelingController.java
@@ -0,0 +1,39 @@
+package br.com.edu.fatec.IPEMControl.Controller;
+
+import br.com.edu.fatec.IPEMControl.DTO.FuelingDTO;
+import br.com.edu.fatec.IPEMControl.DTO.FuelingHistoryDTO;
+import br.com.edu.fatec.IPEMControl.DTO.SavedFuelingDTO;
+import br.com.edu.fatec.IPEMControl.Service.FuelingService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/fueling")
+public class FuelingController {
+
+    private final FuelingService fuelingService;
+
+    public FuelingController(FuelingService fuelingService) {
+        this.fuelingService = fuelingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<SavedFuelingDTO> createFueling(@Valid @RequestBody FuelingDTO dto) {
+        SavedFuelingDTO savedFueling = fuelingService.save(dto);
+        return ResponseEntity
+                .created(URI.create("/fueling/" + savedFueling.id()))
+                .body(savedFueling);
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<FuelingHistoryDTO>> getFuelingHistory(
+            @RequestParam(required = false) Integer vehicleId) {
+
+        List<FuelingHistoryDTO> history = fuelingService.findHistory(vehicleId);
+        return ResponseEntity.ok(history);
+    }
+}
