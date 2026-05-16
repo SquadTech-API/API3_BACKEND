ce602473901906236e392eb8a473330890d45c69
ana-franca-01 <franacaleticiaana@gmail.com>
2026-05-16 17:30:26 -0300

    refactor(#05.3): refatoração da classe UsageHistoryRepository

--- src/main/java/br/com/edu/fatec/IPEMControl/Repository/UsageHistoryRepository.java
+++ src/main/java/br/com/edu/fatec/IPEMControl/Repository/UsageHistoryRepository.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface HistoricoUsoRepository extends JpaRepository<VehicleUsageHistory, Integer> {
+public interface UsageHistoryRepository extends JpaRepository<VehicleUsageHistory, Integer> {
 
     // Busca o histórico filtrando pelo ID do veículo e ordenando pela data mais recente
-    List<VehicleUsageHistory> findByVeiculoIdVeiculoOrderByDataRegistroDesc(Integer idVeiculo);
+    List<VehicleUsageHistory> findByVeiculoIdVeiculoOrderByDataRegistroDesc(Integer vehicleId);
 }
